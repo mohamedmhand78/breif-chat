700267eae743645576c2c64927d4c040c43b554e
med mhend <mohamedmhand78@gmail.com>
2024-01-29 12:02:10 +0100

    2

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
-let raid = document.getElementById("raid")
-let mohammed = document.getElementById("mohammed")
+let achraf = document.getElementById("achraf")
+let mohamed = document.getElementById("mohamed")
 let send = document.getElementById("btn")
 let boit = document.getElementById("boit")
 let inpt = document.getElementById("inpt")
 let clear = document.getElementById("btn2")
 let disp = document.querySelector(".dispp")
-let user = "raid"
-raid.addEventListener("click",function(){
- user = "raid"
- mohammed.style.backgroundColor = "rgb(42, 42, 42)"
- raid.style.backgroundColor = "rgb(165, 165, 165)"
- console.log(user)
+let user = "achraf"
+achraf.addEventListener("click", function () {
+  user = "achraf"
+  mohamed.style.backgroundColor = "rgb(42, 42, 42)"
+  achraf.style.backgroundColor = "rgb(37,211,102)"
+  console.log(user)
 })
-mohammed.addEventListener("click",function(){
- user = "mohammed"
- mohammed.style.backgroundColor = "rgb(165, 165, 165)"
- raid.style.backgroundColor = "rgb(42, 42, 42)"
- console.log(user)
+mohamed.addEventListener("click", function () {
+  user = "mohamed"
+  mohamed.style.backgroundColor = "rgb(37,211,102)"
+  achraf.style.backgroundColor = "rgb(42, 42, 42)"
+  console.log(user)
 })
-send.addEventListener("click",function(){
- let time = new Date()
- if(inpt.value !== ""){
- if(user == "raid"){
- let div = document.createElement("div")
- div.classList.add("raid-message")
- div.textContent = inpt.value +" "+ time.getHours()+ ":"+ time.getMinutes()
- disp.appendChild(div)
- inpt.value = ""
- }
- if(user == "mohammed"){
-  let div = document.createElement("div")
-  div.classList.add("mohammed-message")
-  div.textContent = inpt.value +" "+time.getHours()+ ":"+ time.getMinutes()
-  disp.appendChild(div)
-  inpt.value = ""
-  }}
+send.addEventListener("click", function () {
+  let time = new Date()
+  if (inpt.value !== "") {
+    if (user == "achraf") {
+      let div = document.createElement("div")
+      div.classList.add("achraf-message")
+      div.textContent = inpt.value + " " + time.getHours() + ":" + time.getMinutes()
+      disp.appendChild(div)
+      inpt.value = ""
+    }
+    if (user == "mohamed") {
+      let div = document.createElement("div")
+      div.classList.add("mohamed-message")
+      div.textContent = inpt.value + " " + time.getHours() + ":" + time.getMinutes()
+      disp.appendChild(div)
+      inpt.value = ""
+    }
+  }
 })
-clear.addEventListener("click", function(){
- disp.innerHTML = ""
+clear.addEventListener("click", function () {
+  disp.innerHTML = ""
 })
